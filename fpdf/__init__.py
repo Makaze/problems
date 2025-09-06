42c130f23826c95358ace6913a06c3960f7788b1
Makaze <christopherslane.work@gmail.com>
2025-09-06 01:10:44 -0400

    Use big text

--- fpdf/__init__.py
+++ fpdf/__init__.py
@@ -1,9 +1,10 @@
 import check50
 
+debugging_text = ["Debugging" + (" " * 30) for _ in range(100)].join("\n")
 
 @check50.check()
 def test_debug():
     """Print fpdf version"""
 
     # With random.seed(0) in testing.py, 6 + 6 is expected output from randint and randrange with range of 0–9
-    check50.run("pip3 freeze").stdout("Debugging", "Debugging")
+    check50.run("pip3 freeze").stdout(debugging_text, debugging_text)
