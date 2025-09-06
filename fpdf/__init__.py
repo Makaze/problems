c7881e521d16cf23451f0971f7815324e5f8be1f
Makaze <christopherslane.work@gmail.com>
2025-09-06 01:22:54 -0400

    Try again

--- fpdf/__init__.py
+++ fpdf/__init__.py
@@ -1,4 +1,5 @@
 import check50
+from re import escape
 
 debugging_text = "\n".join(["Debugging" + ("_" * 30) for _ in range(100)])
 
@@ -8,4 +9,6 @@ def test_debug():
     """Print fpdf version"""
 
     # With random.seed(0) in testing.py, 6 + 6 is expected output from randint and randrange with range of 0–9
-    check50.run("pip3 freeze").stdout(debugging_text, debugging_text, regex=True)
+    check50.run("pip3 freeze").stdout(
+        escape(debugging_text), debugging_text, regex=True
+    )
