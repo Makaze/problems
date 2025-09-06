8f373756455b32214dc9c2f14fb5768d6caf7cbb
Makaze <christopherslane.work@gmail.com>
2025-09-06 01:17:22 -0400

    Test with underscores

--- fpdf/__init__.py
+++ fpdf/__init__.py
@@ -1,6 +1,6 @@
 import check50
 
-debugging_text = "\n".join(["Debugging" + (" " * 30) for _ in range(100)])
+debugging_text = "\n".join(["Debugging" + ("_" * 30) for _ in range(100)])
 
 
 @check50.check()
@@ -8,4 +8,7 @@ def test_debug():
     """Print fpdf version"""
 
     # With random.seed(0) in testing.py, 6 + 6 is expected output from randint and randrange with range of 0–9
-    check50.run("pip3 freeze").stdout(debugging_text, debugging_text)
+    check50.run("pip3 freeze").stdout(
+        debugging_text,
+        debugging_text,
+    )
