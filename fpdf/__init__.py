72b6b8693f91057d79e079bab3b7433e2f94d927
Makaze <christopherslane.work@gmail.com>
2025-09-06 01:05:49 -0400

    Test full output

--- fpdf/__init__.py
+++ fpdf/__init__.py
@@ -6,4 +6,4 @@ def test_debug():
     """Print fpdf version"""
 
     # With random.seed(0) in testing.py, 6 + 6 is expected output from randint and randrange with range of 0–9
-    check50.run("pip3 freeze").stdout("Debugging", "Debugging", regex=True)
+    check50.run("pip3 freeze").stdout("Debugging", "Debugging")
